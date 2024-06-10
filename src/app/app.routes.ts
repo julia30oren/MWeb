1879e5cc1336c57038badda16c8a8a458ac6a7ce
Yulia <yuliaoren89@gmail.com>
2024-06-10 12:14:37 -0400

    new1

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import {FirstComponent} from './components/first/first.component';
+import {SecondComponent} from './components/second/second.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'first-component', component: FirstComponent },
+    { path: 'second-component', component: SecondComponent },
+    { path: '',   redirectTo: '/first-component', pathMatch: 'full' }, // redirect to `first-component`
+    { path: '**', 
+        loadComponent: () =>
+            import('./components/pnf/pnf.component').then((c)=>c.PnfComponent), // lazy load of component
+     },  // Wildcard route for a 404 page
+  ];
